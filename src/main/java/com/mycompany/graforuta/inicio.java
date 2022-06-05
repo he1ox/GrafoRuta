baad8c1aab23bd092e936fa6b29942f92d76c649
Jorge <jlopezgarciagt@gmail.com>
2022-06-04 21:46:10 -0600

    primer test funcionando

--- src/main/java/com/mycompany/graforuta/inicio.java
+++ src/main/java/com/mycompany/graforuta/inicio.java
@@ -14,8 +14,8 @@ import GrafosPkg.Grafo;
 public class inicio {
     
     public static void main(String[] args) {
-          Grafo g = new Grafo("abcdef");
-        g.agregarRuta('a','b', 3);
+          Grafo g = new Grafo("iabcdeft");
+/*        g.agregarRuta('a','b', 3);
         g.agregarRuta('a','e', 6);
         g.agregarRuta('a','f',10);
         g.agregarRuta('b','c', 5);
@@ -24,9 +24,30 @@ public class inicio {
         g.agregarRuta('c','e', 9);
         g.agregarRuta('c','f', 7);
         g.agregarRuta('d','f', 4);
-        g.agregarRuta('e','f', 4);
-        char inicio = 'a';
-        char fin    = 'd';
+        g.agregarRuta('e','f', 4);*/
+
+        g.agregarRuta('i','a',9);
+        g.agregarRuta('i','c',2);
+
+        g.agregarRuta('a','d',1);
+
+        g.agregarRuta('b','a',1);
+        g.agregarRuta('b','d',7);
+        g.agregarRuta('b','f',6);
+
+        g.agregarRuta('c','b',1);
+        g.agregarRuta('c','f',10);
+
+        g.agregarRuta('d','e',2);
+        g.agregarRuta('d','t',8);
+
+        g.agregarRuta('e','t',9);
+
+        g.agregarRuta('f','t',2);
+
+        char inicio = 'i';
+        char fin    = 't';
+
         String respuesta = g.encontrarRutaMinimaDijkstra(inicio, fin);
         System.out.println(respuesta);
     }
